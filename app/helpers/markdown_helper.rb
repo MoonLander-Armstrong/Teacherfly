aa0f05f6537a76787d3217f6084884a47a25395f
Tgo Wang <ben2021345@hotmail.com>
2022-05-14 04:30:42 +0800

    style: markdown render styles

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -2,7 +2,7 @@
 
 module MarkdownHelper
   def markdown(content)
-    tag.article data: { controller: 'markdown-viewer' } do
+    tag.article data: { controller: 'markdown-viewer' }, class: "markdown" do
       content
     end
   end
