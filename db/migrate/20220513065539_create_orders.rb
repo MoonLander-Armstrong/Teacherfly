1eeb2a67cf1b13bbaaf0b2330f65607c35fe53d2
hausn <lalalahsuan@gmail.com>
2022-05-13 15:24:23 +0800

    feat model:Order

---
+++ db/migrate/20220513065539_create_orders.rb
@@ -0,0 +1,14 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      t.integer :price
+      t.string :name
+      t.string :email
+      t.string :slug
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :course, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
