208254a7563e0b4de80fbb7539aa7c87b51fd05f
hausn <lalalahsuan@gmail.com>
2022-05-16 18:09:41 +0800

    金流功能-完成進入付款頁面

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,8 @@ class OrdersController < ApplicationController
 
   def index
   end
+
+  def payment
+    @form_info = Newebpay::Mpg.new.form_info
+  end
 end
---
+++ app/service/newebpay/mpg_response.rb
@@ -0,0 +1,53 @@
+module Newebpay
+  class MpgResponse
+    attr_reader :status, :message, :result, :order_no, :trans_no
+
+    def initialize
+      @key = ENV["newebpay_key"]
+      @iv = ENV["newebpay_iv"]
+
+      response = decrypy
+      @status = response['Status']
+      @message = response['Message']
+      @result = response['Result']
+      @order_no = @result["MerchantOrderNo"]
+      @trans_no = @result["TradeNo"]
+      @amt = @result['Amt']
+      @pay_time = @result['PayTime']
+      @card_last4num = @result['Card4No']
+
+    end
+
+    def success?
+      status === 'SUCCESS'
+    end
+
+
+    private
+    # AES 解密
+    def decrypy(encrypted_data)
+      encrypted_data = [encrypted_data].pack('H*')
+      decipher = OpenSSL::Cipher::AES256.new(:CBC)
+      decipher.decrypt
+      decipher.padding = 0
+      decipher.key = @key
+      decipher.iv = @iv
+      data = decipher.update(encrypted_data) + decipher.final
+      raw_data = strippadding(data)
+      JSON.parse(raw_data)
+    end
+
+    def strippadding(data)
+      slast = data[-1].ord
+      slastc = slast.chr
+      string_match = /#{slastc}{#{slast}}/ =~ data
+      if !string_match.nil?
+        data[0, string_match]
+      else
+        false
+      end
+    end
+
+
+  end
+end
