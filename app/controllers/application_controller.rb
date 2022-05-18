955cb4841def2088ee312c1a0c3ad9fb9c6c7a7b
Tgo Wang <ben2021345@hotmail.com>
2022-05-18 17:55:44 +0800

    revise: code and unused pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   def after_sign_in_path_for(resource)
     owner_courses_path
   end
@@ -13,5 +14,11 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  private
+
+  def not_found
+    render file: "#{Rails.root}/public/404.html", status: 404, layout: false
   end
 end
